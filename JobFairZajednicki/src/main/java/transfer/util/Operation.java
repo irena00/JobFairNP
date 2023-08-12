698b28d71dc794e54d54f314617b84103f4da830
irena00 <randjeloviceva13@gmail.com>
2023-08-12 15:48:45 +0200

    Dodat paket "transfer.util" i u njemu napravljen interfejs "Operation".

---
+++ JobFairZajednicki/src/main/java/transfer/util/Operation.java
@@ -0,0 +1,25 @@
+package transfer.util;
+
+public interface Operation {
+	public static final int LOGIN = 0;
+
+    public static final int GET_ALL_ADMINISTRATOR = 1;
+
+    public static final int ADD_SASTANAK = 2;
+    public static final int DELETE_SASTANAK = 3;
+    public static final int UPDATE_SASTANAK = 4;
+    public static final int GET_ALL_SASTANAK = 5;
+
+    public static final int ADD_UGOVOR = 6;
+    public static final int DELETE_UGOVOR = 7;
+    public static final int UPDATE_UGOVOR = 8;
+    public static final int GET_ALL_UGOVOR = 9;
+
+    public static final int GET_ALL_STAVKA_UGOVORA = 10;
+
+    public static final int GET_ALL_CLAN_CR_TIMA = 11;
+
+    public static final int GET_ALL_KOMPANIJA = 12;
+
+    public static final int GET_ALL_PAKET = 13;
+}

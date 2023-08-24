06f8647a4ef97017cd2dacb0da3026c415365de6
irena00 <randjeloviceva13@gmail.com>
2023-08-24 23:00:32 +0200

    Dodatne izmene

---
+++ JobFairServer/src/test/java/so/administrator/SOGetAllAdministratorTest.java
@@ -0,0 +1,14 @@
+package so.administrator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SOGetAllAdministratorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
